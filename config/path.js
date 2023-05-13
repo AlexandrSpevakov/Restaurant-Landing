ad6cefadcf1c0bcc00aef4869fc6c1fa81942a62
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-05-13 04:55:06 -0400

    fix: try to exclude favicon from the gulp flow

--- config/path.js
+++ config/path.js
@@ -1,42 +1,45 @@
-const pathSrc = './src';
-const pathDest = './public';
+const pathSrc = "./src";
+const pathDest = "./public";
 
 export default {
-   root: pathDest,
+  root: pathDest,
 
-   html: {
-      src: pathSrc + '/html/*.html',
-      watch: pathSrc + '/html/**/*.html',
-      dest: pathDest
-   },
+  html: {
+    src: pathSrc + "/html/*.html",
+    watch: pathSrc + "/html/**/*.html",
+    dest: pathDest,
+  },
 
-   css: {
-      src: pathSrc + '/css/*.css',
-      watch: pathSrc + '/css/**/*.css',
-      dest: pathDest + '/css'
-   },
+  css: {
+    src: pathSrc + "/css/*.css",
+    watch: pathSrc + "/css/**/*.css",
+    dest: pathDest + "/css",
+  },
 
-   scss: {
-      src: pathSrc + '/scss/*.{scss,sass}',
-      watch: pathSrc + '/scss/**/*.{scss,sass}',
-      dest: pathDest + '/css'
-   },
+  scss: {
+    src: pathSrc + "/scss/*.{scss,sass}",
+    watch: pathSrc + "/scss/**/*.{scss,sass}",
+    dest: pathDest + "/css",
+  },
 
-   js: {
-      src: pathSrc + '/js/*.js',
-      watch: pathSrc + '/js/**/*.js',
-      dest: pathDest + '/js'
-   },
+  js: {
+    src: pathSrc + "/js/*.js",
+    watch: pathSrc + "/js/**/*.js",
+    dest: pathDest + "/js",
+  },
 
-   ts: {
-      src: pathSrc + '/ts/*.ts',
-      watch: pathSrc + '/ts/**/*.ts',
-      dest: pathDest + '/js'
-   },
+  ts: {
+    src: pathSrc + "/ts/*.ts",
+    watch: pathSrc + "/ts/**/*.ts",
+    dest: pathDest + "/js",
+  },
 
-   img: {
-      src: pathSrc + '/img/*.{png,jpg,jpeg,gif,svg}',
-      watch: pathSrc + '/img/**/*.{png,jpg,jpeg,gif,svg}',
-      dest: pathDest + '/img'
-   }
-}
+  img: {
+    src: [
+      pathSrc + "/img/*.{png,jpg,jpeg,gif,svg}",
+      "!" + pathSrc + "/img/ico.png",
+    ],
+    watch: pathSrc + "/img/**/*.{png,jpg,jpeg,gif,svg}",
+    dest: pathDest + "/img",
+  },
+};
